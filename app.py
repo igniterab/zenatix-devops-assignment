d29fac8e4abfc440006c72eb431f70215a20d81c
igniterab <56384941+igniterab@users.noreply.github.com>
2021-07-15 19:33:44 +0530

    Added python file for data collection

--- app.py
+++ app.py
@@ -5,12 +5,16 @@ from elasticsearch import Elasticsearch
 es = Elasticsearch()
 
 cmd = "ps -eo pid,%mem,%cpu"
+
+# Contains stat of per process
 indexname = "os-stat-data"
+
+# Contains stat for whole system
 indexname2="os-stat-system-data"
 
 
 def get_doc(pid, mem, cpu, now):
-    return {
+    return { # Creating dictionary of data
         "timestamp": now,
         "cpu": float(cpu),
         "mem": float(mem),
@@ -19,23 +23,23 @@ def get_doc(pid, mem, cpu, now):
 
 
 def process_line(line):
-    sp = line.split()
+    sp = line.split() # Splits line into columns
     now = datetime.now()
     return get_doc(sp[0], sp[1], sp[2], now)
 
 
 while True:
 
-    out = subprocess.getoutput(cmd)
-    lines = out.splitlines()
+    out = subprocess.getoutput(cmd) # Execution of command for retriving process stats
+    lines = out.splitlines() # Splitting output by lines
     cpu = 0
     mem = 0
     for x in map(process_line, out.splitlines()[1:]):
-        es.index(index=indexname, body=x)
+        es.index(index=indexname, body=x) # Pushing data to elastisearch
+        # print("Pushed") 
         cpu += x["cpu"]
         mem += x["mem"]
     es.index(index=indexname2, body={"mem": mem,
                                     "cpu": cpu, "timestamp": datetime.now()})
 
-    time.sleep(1)
-
+    time.sleep(1) # Waiting for a sec
